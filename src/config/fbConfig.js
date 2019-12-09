8458bab86b9e58221f3f15e61a10530676952437
SlackEight <morgan.blem@gmail.com>
2019-12-09 21:45:13 +0200

    Completed lesson 32

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -5,13 +5,13 @@ import 'firebase/auth';
 // Replace this with your own config details
 var config = {
   apiKey: "AIzaSyDJ7CoE4zp6NuBrMC-t0-mejOfUIGP1T1M",
-    authDomain: "zaio-mario-plan.firebaseapp.com",
-    databaseURL: "https://zaio-mario-plan.firebaseio.com",
-    projectId: "zaio-mario-plan",
-    storageBucket: "zaio-mario-plan.appspot.com",
-    messagingSenderId: "701188575083",
-    appId: "1:701188575083:web:0bdd6786a298118f3d462e",
-    measurementId: "G-NRTQ72V9QQ"
+  authDomain: "zaio-mario-plan.firebaseapp.com",
+  databaseURL: "https://zaio-mario-plan.firebaseio.com",
+  projectId: "zaio-mario-plan",
+  storageBucket: "zaio-mario-plan.appspot.com",
+  messagingSenderId: "701188575083",
+  appId: "1:701188575083:web:0bdd6786a298118f3d462e",
+  measurementId: "G-NRTQ72V9QQ"
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({ timestampsInSnapshots: true });
