a1a3e6b7b7e4b75c5d3c036aabee785ec10bf251
Morgan Blem <morgan.blem@gmail.com>
2019-12-09 17:18:49 +0200

    now loads projects from firebase

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,8 +5,8 @@ const ProjectSummary = ({project}) => {
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title ">{project.title}</span>
-        <p>Posted by Morgan</p>
-        <p className="grey-text">9th December, 4pm</p>
+        <p>Posted by The Net Ninja</p>
+        <p className="grey-text">3rd September, 2am</p>
       </div>
     </div>
   )
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,17 +1,16 @@
-const initState = {
-  projects: [
-    {id: '1', title: 'Please help, tripping on Mario Mushrooms', content: 'I took a red and white mushroom and now I\'m 10 feet tall, so I keep tripping on everything!'},
-    {id: '2', title: 'End Bowser once and for all', content: 'Shh.. Meet by the bridge..'},
-    {id: '3', title: 'Find out who Luigi\'s brother is', content: 'I have my suspicions...'}
-  ]
-}
+const initState = {}
 
 const projectReducer = (state = initState, action) => {
   switch (action.type) {
-    case 'CREATE_PROJECT':
-      console.log('create project', action.project);
+    case 'CREATE_PROJECT_SUCCESS':
+      console.log('create project success');
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create project error');
+      return state;
+    default:
+      return state;
   }
-  return state;
 };
 
 export default projectReducer;
